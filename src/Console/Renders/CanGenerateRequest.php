645863b66af3f08bea1a37541598a3cff5a75c9c
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2017-01-27 14:38:38 +0100

    Form Request generator trait

---
+++ src/Console/Renders/CanGenerateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AbdelilahLbardi\LaraGenerator\Console\Renders;
+
+use Illuminate\Support\Facades\File;
+
+trait CanGenerateRequest{
+
+	public function requestBlock()
+	{
+		   
+	}
+
+	/*
+	* Make namespace Folder
+	*/
+	public function makeRequestFolder()
+	{
+		
+	}
+
+	/*
+	* Get the template and replace the expressions
+	*/
+	public function getRequestContent()
+	{
+		
+	}
+
+	/*
+	* Create the rendered template from setContent
+	*/
+	public function createRequestFile($content)
+	{
+
+	}
+
+
+}
